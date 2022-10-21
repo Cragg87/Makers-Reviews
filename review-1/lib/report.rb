cb17443f7c1530a45658bedc12ee66ca142dde8e
Miles Cragg <milescragg@gmail.com>
2022-10-21 11:43:48 +0100

    Adds code which returns client's example

--- review-1/lib/report.rb
+++ review-1/lib/report.rb
@@ -1,8 +1,19 @@
 def report(string)
-  word_count = string.split(", ")
-  if word_count.length == 1
-    return "#{string}: 1"
-  elsif string == "Green, Green"
-    return "Green: 2"
+  red = []
+  amber = []
+  green = []
+  words = string.split(", ")
+  words.each do |word|
+    if word == "Green"
+      green << word
+    elsif word == "Amber"
+      amber << word
+    elsif word == "Red"
+      red << word
+    end
   end
+  red_number = red.length
+  amber_number = amber.length
+  green_number = green.length
+  return "Green: #{green_number}\nAmber: #{amber_number}\nRed: #{red_number}"
 end
--- review-1/spec/report_spec.rb
+++ review-1/spec/report_spec.rb
@@ -1,27 +1,27 @@
 require_relative '../lib/report.rb'
 
 RSpec.describe 'report' do
-  it 'returns "Green: 1' do
-    expect(report("Green")).to eq "Green: 1"
+  it 'returns results containing "Green: 1' do
+    expect(report("Green")).to eq "Green: 1\nAmber: 0\nRed: 0"
   end
 
-  it 'returns "Amber: 1"' do
+  it 'returns results containing "Amber: 1"' do
     string = "Amber"
-    expect(report(string)).to eq "Amber: 1"
+    expect(report(string)).to eq "Green: 0\nAmber: 1\nRed: 0"
   end
 
-  it 'returns "Red: 1"' do
+  it 'returns results containing "Red: 1"' do
     string = "Red"
-    expect(report(string)).to eq "Red: 1"
+    expect(report(string)).to eq "Green: 0\nAmber: 0\nRed: 1"
   end
 
-  it 'returns "Green: 2"' do
+  it 'returns results containing "Green: 2"' do
     string = "Green, Green"
-    expect(report(string)).to eq "Green: 2"
+    expect(report(string)).to eq "Green: 2\nAmber: 0\nRed: 0"
   end
 
-  it 'returns "Amber: 2"' do
-    string = "Amber, Amber"
-    expect(report(string)).to eq "Amber: 2"
+  it 'returns "Green: 2\nAmber: 1\nRed: 2"' do
+    string = "Green, Green, Red, Amber, Red"
+    expect(report(string)).to eq "Green: 2\nAmber: 1\nRed: 2"
   end
 end
